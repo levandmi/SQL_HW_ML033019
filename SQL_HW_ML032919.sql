b219b3c47bb4576abcf043a5b897996b35c0a2ac
Levandoski <Levanm1@labcorp.com>
2019-04-02 17:59:21 -0400

    Cleaned HW.

--- SQL_HW_ML032919.sql
+++ SQL_HW_ML032919.sql
@@ -1,47 +1,51 @@
 use sakila;
 
--- 1a
+-- 1a. Display the first and last names of all actors from the table actor.
 SELECT first_name, last_name FROM actor;
 
--- 1b
+-- 1b. Display the first and last name of each actor in a single column in upper case letters. Name the column Actor Name.
 SELECT UPPER(concat(first_name, " ", last_name)) as "Joined_Name" FROM actor;
 
--- 2a
+-- 2a. You need to find the ID number, first name, and last name of an actor, 
+-- of whom you know only the first name, "Joe." What is one query would you use to obtain this information?
 SELECT * FROM actor WHERE first_name = "JOE";
 
--- 2b
+-- 2b. Find all actors whose last name contain the letters GEN:
 SELECT * FROM actor WHERE last_name LIKE '%GEN%';
 
--- 2c
+-- 2c. Find all actors whose last names contain the letters LI. This time, order the rows by last name and first name, in that order:
 SELECT * FROM actor WHERE last_name LIKE '%LI%'
 ORDER BY last_name, first_name;
 
--- 2d
+-- 2d. Using IN, display the country_id and country columns of the following countries: Afghanistan, Bangladesh, and China:
 SELECT country_id, country FROM  country 
 WHERE country in ("Afghanistan", "Bangladesh", "China");
 
--- 3a 
+-- 3a. You want to keep a description of each actor. You don't think you will be performing queries on a description, 
+-- so create a column in the table actor named description and use the data type BLOB (Make sure to research the type BLOB, 
+-- as the difference between it and VARCHAR are significant)
 ALTER TABLE actor ADD description BLOB;
 
--- 3b
+-- 3b. Very quickly you realize that entering descriptions for each actor is too much effort. Delete the description column.
 ALTER TABLE actor DROP COLUMN description;
 
--- 4a
+-- 4a. List the last names of actors, as well as how many actors have that last name.
 SELECT last_name, count(last_name) from actor group by last_name;
 
--- 4b
+-- 4b. List last names of actors and the number of actors who have that last name, but only for names that are shared by at least two actors
 SELECT last_name, count(last_name) from actor group by last_name having count(last_name) >= 2;
 
--- 4c
+-- 4c. The actor HARPO WILLIAMS was accidentally entered in the actor table as GROUCHO WILLIAMS. Write a query to fix the record.
 UPDATE actor set first_name = "HARPO"  WHERE first_name = "GROUCHO" AND last_name = "WILLIAMS";
 
--- 4d
+-- 4d. Perhaps we were too hasty in changing GROUCHO to HARPO. It turns out that GROUCHO was the correct name after all! 
+-- In a single query, if the first name of the actor is currently HARPO, change it to GROUCHO.
 UPDATE actor set first_name = "GROUCHO"  WHERE first_name = "HARPO" AND last_name = "WILLIAMS";
 
--- 5a
+-- 5a. You cannot locate the schema of the address table. Which query would you use to re-create it?
 show create table address;
 
--- 6a
+-- 6a. Use JOIN to display the first and last names, as well as the address, of each staff member. Use the tables staff and address:
 SELECT  staff.first_name, staff.last_name, address.address
 FROM staff
 INNER JOIN address ON
@@ -152,8 +156,6 @@ SELECT * FROM Answer_8a;
 -- 8c. You find that you no longer need the view top_five_genres. Write a query to delete it.
 DROP VIEW answer_8a;
 
-select * from rental;
-select * from payment;
 
  
 
